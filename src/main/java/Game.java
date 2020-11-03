5b856b3d7655bf8c8fff755892173dc1d11ee0f1
mateuszkowalczykk <x625134x@gmail.com>
2020-11-03 17:38:01 +0100

    fixup! add count score in any moment of game

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -10,20 +10,21 @@ public class Game {
 
     public Game() {
         currentFrameNumber = 1;
-        framesMap.put(currentFrameNumber,new ArrayList<>());
+        framesMap.put(currentFrameNumber, new ArrayList<>());
     }
 
-    public void roll(int knockedPins){
-        if(currentFrameNumber != numberOfFrames){
+    public void roll(int knockedPins) {
+        if (currentFrameNumber != numberOfFrames) {
             addRollToFramesMap(knockedPins);
-        }else{
+        } else {
             addRollToFramesMapInLastFrame(knockedPins);
         }
     }
 
-    public int score(){
-        for(int i = 1 ;i<=framesMap.size();i++){
-            if(framesMap.get(i).size() >0){
+    public int score() {
+        subScoresList.clear();
+        for (int i = 1; i <= framesMap.size(); i++) {
+            if (framesMap.get(i).size() > 0) {
                 int subScore = 0;
                 subScore += listSum(framesMap.get(i));
                 subScore += strikeBonus(i);
@@ -32,73 +33,72 @@ public class Game {
             }
         }
 //        System.out.println(framesMap);
-        System.out.println(subScoresList);
+//        System.out.println(subScoresList);
         return listSum(subScoresList);
     }
 
-    private int spareBonus(int frameNumber){
-        if(isSpare(framesMap.get(frameNumber)) && frameNumber != numberOfFrames && framesMap.get(frameNumber+1).size() > 0){
-            return framesMap.get(frameNumber+1).get(0);
+    private int spareBonus(int frameNumber) {
+        if (isSpare(framesMap.get(frameNumber)) && frameNumber != numberOfFrames && framesMap.get(frameNumber + 1).size() > 0) {
+            return framesMap.get(frameNumber + 1).get(0);
         }
         return 0;
     }
 
-    private int strikeBonus(int frameNumber){
-        if(isStrike(framesMap.get(frameNumber)) && frameNumber != numberOfFrames){
-            if(framesMap.get(frameNumber+1).size()>1 && framesMap.get(frameNumber+1).size() > 1){//next roll isn't strike
-                return framesMap.get(frameNumber+1).get(0) + framesMap.get(frameNumber+1).get(1);
-            }else{                               //next roll is strike
-                if(framesMap.get(frameNumber+2) != null){
-                    return framesMap.get(frameNumber+1).get(0)+framesMap.get(frameNumber+2).get(0);
+    private int strikeBonus(int frameNumber) {
+        if (isStrike(framesMap.get(frameNumber)) && frameNumber != numberOfFrames) {
+            if (framesMap.get(frameNumber + 1).size() > 1 && framesMap.get(frameNumber + 1).size() > 1) {//next roll isn't strike
+                return framesMap.get(frameNumber + 1).get(0) + framesMap.get(frameNumber + 1).get(1);
+            } else {                               //next roll is strike
+                if (framesMap.get(frameNumber + 2) != null) {
+                    return framesMap.get(frameNumber + 1).get(0) + framesMap.get(frameNumber + 2).get(0);
                 }
             }
         }
         return 0;
     }
 
-    private void addRollToFramesMap(int knockedPins){
+    private void addRollToFramesMap(int knockedPins) {
         ArrayList<Integer> currentFrame = framesMap.get(currentFrameNumber);
 
-        if(currentFrame.size() == 0){
+        if (currentFrame.size() == 0) {
             currentFrame.add(knockedPins);
-            if(isStrike(currentFrame)){
+            if (isStrike(currentFrame)) {
                 nextFrame();
             }
-        }else{
+        } else {
             currentFrame.add(knockedPins);
             nextFrame();
         }
     }
 
-    private void addRollToFramesMapInLastFrame(int knockedPins){
+    private void addRollToFramesMapInLastFrame(int knockedPins) {
         ArrayList<Integer> currentFrame = framesMap.get(currentFrameNumber);
 
-        if(currentFrame.size()<=1){
+        if (currentFrame.size() <= 1) {
             currentFrame.add(knockedPins);
-        }else if((isStrike(currentFrame)||isSpare(currentFrame))&&currentFrame.size()==2){
+        } else if ((isStrike(currentFrame) || isSpare(currentFrame)) && currentFrame.size() == 2) {
             currentFrame.add(knockedPins);
         }
     }
 
-    private void nextFrame(){
-        if(currentFrameNumber<numberOfFrames){
+    private void nextFrame() {
+        if (currentFrameNumber < numberOfFrames) {
             currentFrameNumber++;
-            framesMap.put(currentFrameNumber,new ArrayList<>());
+            framesMap.put(currentFrameNumber, new ArrayList<>());
         }
     }
 
-    private boolean isStrike(ArrayList<Integer> frame){
+    private boolean isStrike(ArrayList<Integer> frame) {
         return frame.get(0) == 10;
     }
 
-    private boolean isSpare(ArrayList<Integer> frame){
+    private boolean isSpare(ArrayList<Integer> frame) {
         return listSum(frame) == 10 && frame.size() > 1;
     }
 
-    private int listSum(ArrayList<Integer> frame){
+    private int listSum(ArrayList<Integer> frame) {
         return frame.stream()
                 .reduce(Integer::sum)
                 .get();
     }
-
 }
