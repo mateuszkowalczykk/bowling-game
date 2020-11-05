f1a71083eb6478e59bceb2807831b60c0902c00b
mateuszkowalczykk <x625134x@gmail.com>
2020-11-05 15:36:58 +0100

    correcting code, add input validation(with check possible number of pins left to knock) and endGame detect

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -5,95 +5,94 @@ import java.util.Map;
 public class Game {
     private final int numberOfFrames = 10;
     private final Map<Integer, ArrayList<Integer>> framesMap = new HashMap<>();
-    private final ArrayList<Integer> subScoresList = new ArrayList<>();
-    private int currentFrameNumber;
+    private int currentFrameNumber = 1;
+    private boolean isEndGame = false;
 
     public Game() {
-        currentFrameNumber = 1;
         framesMap.put(currentFrameNumber, new ArrayList<>());
     }
 
-    public void roll(int knockedPins) {
+    public boolean roll(int knockedPins) {
+
+        if(!isEndGame){
+            if(isRollPossible(knockedPins)){
+                framesMap.get(currentFrameNumber).add(knockedPins);
+            }else{
+                System.out.println("This roll is not possible. Please try again.");
+            }
+        }
+
         if (currentFrameNumber != numberOfFrames) {
-            addRollToFramesMap(knockedPins);
-        } else {
-            addRollToFramesMapInLastFrame(knockedPins);
-        }
-    }
-
-    public int score() {
-        subScoresList.clear();
-        for (int i = 1; i <= framesMap.size(); i++) {
-            if (framesMap.get(i).size() > 0) {
-                int subScore = 0;
-                subScore += listSum(framesMap.get(i));
-                subScore += strikeBonus(i);
-                subScore += spareBonus(i);
-                subScoresList.add(subScore);
-            }
-        }
-//        System.out.println(framesMap);
-//        System.out.println(subScoresList);
-        return listSum(subScoresList);
-    }
-
-    private int spareBonus(int frameNumber) {
-        if (isSpare(framesMap.get(frameNumber)) && frameNumber != numberOfFrames && framesMap.get(frameNumber + 1).size() > 0) {
-            return framesMap.get(frameNumber + 1).get(0);
-        }
-        return 0;
-    }
-
-    private int strikeBonus(int frameNumber) {
-        if (isStrike(framesMap.get(frameNumber)) && frameNumber != numberOfFrames) {
-            if (framesMap.get(frameNumber + 1).size() > 1 && framesMap.get(frameNumber + 1).size() > 1) {//next roll isn't strike
-                return framesMap.get(frameNumber + 1).get(0) + framesMap.get(frameNumber + 1).get(1);
-            } else {                               //next roll is strike
-                if (framesMap.get(frameNumber + 2) != null) {
-                    return framesMap.get(frameNumber + 1).get(0) + framesMap.get(frameNumber + 2).get(0);
-                }
-            }
-        }
-        return 0;
-    }
-
-    private void addRollToFramesMap(int knockedPins) {
-        ArrayList<Integer> currentFrame = framesMap.get(currentFrameNumber);
-
-        if (currentFrame.size() == 0) {
-            currentFrame.add(knockedPins);
-            if (isStrike(currentFrame)) {
-                nextFrame();
-            }
-        } else {
-            currentFrame.add(knockedPins);
             nextFrame();
+        } else {
+            if(endGameCheck()) return false;
         }
+        return true;
     }
 
-    private void addRollToFramesMapInLastFrame(int knockedPins) {
-        ArrayList<Integer> currentFrame = framesMap.get(currentFrameNumber);
-
-        if (currentFrame.size() <= 1) {
-            currentFrame.add(knockedPins);
-        } else if ((isStrike(currentFrame) || isSpare(currentFrame)) && currentFrame.size() == 2) {
-            currentFrame.add(knockedPins);
+    public int score(){
+        int score = 0;
+        for(int i = 1;i<=numberOfFrames;i++){
+            score += listSum(framesMap.get(i));
+            if(i != numberOfFrames){
+                score += isSpare(framesMap.get(i))? spareBonus(i): 0;
+                score += isStrike(framesMap.get(i))? strikeBonus(i): 0;
+            }
         }
+        return score;
     }
 
     private void nextFrame() {
-        if (currentFrameNumber < numberOfFrames) {
+        ArrayList<Integer> currentFrame = framesMap.get(currentFrameNumber);
+
+        if(isStrike(currentFrame) || currentFrame.size() == 2){
             currentFrameNumber++;
-            framesMap.put(currentFrameNumber, new ArrayList<>());
+            framesMap.put(currentFrameNumber,new ArrayList<>());
+        }
+    }
+
+    private boolean endGameCheck() {
+        ArrayList<Integer> currentFrame = framesMap.get(currentFrameNumber);
+
+        if((currentFrame.size() == 2 && !isSpare(currentFrame) && !isStrike(currentFrame)) || currentFrame.size() == 3) {
+            isEndGame = true;
+            System.out.println("Game over!");
+            return true;
+        }
+        return false;
+    }
+
+    private boolean isRollPossible(int knockedPins)  {
+        ArrayList<Integer> currentFrame = framesMap.get(currentFrameNumber);
+
+        if(knockedPins>10 || knockedPins<0) return false;
+        if(currentFrame.size() == 0) return true;
+        if(currentFrameNumber != numberOfFrames || !isStrike(currentFrame)) {
+            if (currentFrame.size() == 1 && currentFrame.get(0) + knockedPins > 10) return false;
+        }else{
+            if (currentFrame.size() == 2 && currentFrame.get(1) + knockedPins > 10 && currentFrame.get(1) != 10) return false;
+        }
+        return true;
+    }
+
+    private int spareBonus(int frameNumber) {
+        return framesMap.get(frameNumber + 1).get(0);
+    }
+
+    private int strikeBonus(int frameNumber) {
+        if(isStrike(framesMap.get(frameNumber + 1)) && frameNumber != numberOfFrames-1){
+            return framesMap.get(frameNumber + 1).get(0) + framesMap.get(frameNumber + 2).get(0);
+        }else{
+            return framesMap.get(frameNumber + 1).get(0) + framesMap.get(frameNumber + 1).get(1);
         }
     }
 
     private boolean isStrike(ArrayList<Integer> frame) {
-        return frame.get(0) == 10;
+        return frame.size() != 0? frame.get(0) == 10 : false;
     }
 
     private boolean isSpare(ArrayList<Integer> frame) {
-        return listSum(frame) == 10 && frame.size() > 1;
+        return frame.size() != 0? listSum(frame) == 10 && frame.size() > 1 : false;
     }
 
     private int listSum(ArrayList<Integer> frame) {
