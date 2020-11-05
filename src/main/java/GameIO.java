77afe25af742125d7aa1d92d058644fac7964adb
mateuszkowalczykk <x625134x@gmail.com>
2020-11-05 15:38:01 +0100

    add terminal interface to game

---
+++ src/main/java/GameIO.java
@@ -0,0 +1,44 @@
+import Exceptions.ExceptionType;
+import Exceptions.GameException;
+import java.util.Scanner;
+
+
+
+public class GameIO {
+    private static Game game = new Game();
+    private static final Scanner scanner = new Scanner(System.in);
+    private static boolean closeApp = false;
+
+    public static void start(){
+        System.out.println("Welcome in Bowling Game");
+
+        while (!closeApp) {
+            System.out.println("Enter number of knocked pins to add roll (Enter 'e' to exit)");
+            String input = scanner.nextLine();
+
+            if(input.equalsIgnoreCase("e")){
+                closeApp = true;
+            }else{
+                try{
+                    if(!game.roll(Integer.parseInt(input))){
+                        endGame();
+                    }
+                }catch(NumberFormatException e){
+                    System.out.println("Wrong character. Please try again");
+                }
+            }
+        }
+    }
+
+    private static void endGame(){
+        System.out.println("Your score is: " + game.score());
+        System.out.println("Enter 'n' to start new game or any other key to exit");
+        String input = scanner.nextLine();
+        if(input.equalsIgnoreCase("n")){
+            game = new Game();
+        }else{
+            closeApp = true;
+        }
+    }
+
+}
